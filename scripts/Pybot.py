d2c4b8b0e2162ad1e4671f891f98b16e83abfd05
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-10-13 12:13:30 +0200

    Removed greetings. Renamed: getPing to pingStatus, status to statuses. Pingmethod now in dict-initialization. Fix in WarnIfDown-method

--- scripts/Pybot.py
+++ scripts/Pybot.py
@@ -1,12 +1,14 @@
 # coding: utf-8
 __author__ = 'Harald'
 
-import serial
 import socket
 import time
 import os
-from random import randint
 
+import serial
+
+
+discoActive = True
 updateDay = time.strftime("%d")
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
@@ -15,30 +17,29 @@ server = "irc.freenode.net"
 channel = '#tihlde-drift'
 botnick = "hal-9001"
 password = open("pw").read()
-greetings = ["We meet again",
-             "Hello",
-             "Sup",
-             "What's up?",
-             "It's been a long time... not long enough",
-             "Hi!",
-             "My old nemesis",
-             "Did you hear something?",
-             "I think some sub-intelligent species is trying to establish contact..."]
 
-status = {
-    'colargol': 'notYetAvailable',
-    'fantorangen': 'notYetAvailable',
-    'odin': 'notYetAvailable',
-    'coastguard': 'notYetAvailable',
-    'handymanny': 'notYetAvailable',
-    'balthazar': 'notYetAvailable',
-    'thor': 'notYetAvailable',
-    'vcenter.nerdvana': 'notYetAvailable',
-    'bashful.nerdvana': 'notYetAvailable',
-    'dopey.nerdvana': 'notYetAvailable',
-    'grumpy.nerdvana': 'notYetAvailable',
-    'sneezy.nerdvana': 'notYetAvailable',
-    'sleepy.nerdvana': 'notYetAvailable'
+
+def pingStatus(hostname):
+    if os.system("ping -c 1 " + hostname + ".tihlde.org") == 0:
+        return "\x033,1oppe\x03"
+    else:
+        return "\x030,4NEDE\x03"
+
+
+statuses = {
+    'colargol': pingStatus('colargol'),
+    'fantorangen': pingStatus('fantorangen'),
+    'odin': pingStatus('odin'),
+    'coastguard': pingStatus('coastguard'),
+    'handymanny': pingStatus('handymanny'),
+    'balthazar': pingStatus('balthazar'),
+    'thor': pingStatus('thor'),
+    'vcenter.nerdvana': pingStatus('vcenter.nerdvana'),
+    'bashful.nerdvana': pingStatus('bashful.nerdvana'),
+    'dopey.nerdvana': pingStatus('dopey.nerdvana'),
+    'grumpy.nerdvana': pingStatus('grumpy.nerdvana'),
+    'sneezy.nerdvana': pingStatus('sneezy.nerdvana'),
+    'sleepy.nerdvana': pingStatus('sleepy.nerdvana')
 }
 
 
@@ -56,43 +57,40 @@ def sendmsg(chan, msg):
 
 
 def pingServers():
-    sendmsg(channel, "colargol: " + status["colargol"] +
-            "  fantorangen: " + status["fantorangen"] +
-            "  odin: " + status["odin"] +
-            "  coastguard: " + status["coastguard"] +
-            "  handymanny: " + status["handymanny"] +
-            "  balthazar: " + status["balthazar"] +
-            "  thor: " + status["thor"]
+    sendmsg(channel, "colargol: " + statuses["colargol"] +
+            "  fantorangen: " + statuses["fantorangen"] +
+            "  odin: " + statuses["odin"] +
+            "  coastguard: " + statuses["coastguard"] +
+            "  handymanny: " + statuses["handymanny"] +
+            "  balthazar: " + statuses["balthazar"] +
+            "  thor: " + statuses["thor"]
             )
 
 
 def pingNerdvana():
-    sendmsg(channel, "vcenter: " + status["vcenter.nerdvana"] +
-            "  bashful: " + status["bashful.nerdvana"] +
-            "  grumpy: " + status["grumpy.nerdvana"] +
-            "  dopey: " + status["dopey.nerdvana"] +
-            "  sleepy: " + status["sleepy.nerdvana"] +
-            "  sneezy: " + status["sneezy.nerdvana"]
+    sendmsg(channel, "vcenter: " + statuses["vcenter.nerdvana"] +
+            "  bashful: " + statuses["bashful.nerdvana"] +
+            "  grumpy: " + statuses["grumpy.nerdvana"] +
+            "  dopey: " + statuses["dopey.nerdvana"] +
+            "  sleepy: " + statuses["sleepy.nerdvana"] +
+            "  sneezy: " + statuses["sneezy.nerdvana"]
             )
 
 
-def getPing(hostname):
-    if os.system("ping -c 1 " + hostname + ".tihlde.org") == 0:
-        return "\x033,1oppe\x03"
-    else:
-        return "\x030,4NEDE\x03"
-
-
 def updateStatuses():
-    for key in status:
-        status[key] = getPing(key)
+    for key in statuses:
+        statuses[key] = pingStatus(key)
 
 
 def warnIfDown():
     msg = ""
-    for key in status:
-        if status[key].find("NEDE") != -1:
-            msg += key[:key.find(".")] + ": " + status[key] + " \x030,4ER NEDE\x03\n"
+    for key in statuses:
+        if statuses[key].find("NEDE") != -1:
+            if key.find(".") != -1:
+                serverName = key
+            else:
+                serverName = key[:key.find(".")]
+            msg += serverName + ": " + statuses[key] + " \x030,4ER NEDE\x03\n"
     if len(msg) > 0:
         sendmsg(channel, msg)
 
@@ -109,21 +107,27 @@ while 1:
     ircmsg = ircmsg.strip('\n')  # removing linebreaks.
 
     print(ircmsg)
-    
-    if ircmsg.find("PRIVMSG #tihlde-drift") != -1:  # Responds to 'Hello botnick'
-        if ircmsg.lower().find(":hello " + botnick) != -1:
-            send("PRIVMSG " + channel + " :" + greetings[randint(0, len(greetings) - 1)])
-        elif ircmsg.find(".serverstatus") != -1:  # Responds to -serverstatus
+
+    # Make sure the message is in specified channel and not a private msg
+    if ircmsg.find("PRIVMSG #tihlde-drift") != -1:
+        if ircmsg.find(".serverstatus") != -1:  # Respond to .serverstatus
             updateStatuses()
             pingServers()
-        elif ircmsg.find(".nerdvanastatus") != -1:  # Respons to .nerdvanastatus
+
+        if ircmsg.find(".nerdvanastatus") != -1:  # Respond to .nerdvanastatus
             updateStatuses()
             pingNerdvana()
-    if ircmsg.find(".discotime!") != -1:
-        print("Discotime!")
-        ser.write(b'1')
-    else:
-        ser.write(b'0')
+
+        if ircmsg.find(".discoDeactivate") != -1:
+            discoActive = False
+        elif ircmsg.find(".discoActivate") != -1:
+            discoActive = True
+
+        if ircmsg.find(".discotime!") != -1 and discoActive:
+            print("Discotime!")
+            ser.write(b'1')
+        else:
+            ser.write(b'0')
 
     if ircmsg.find("PING :") != -1:  # respond to pings
         send("PONG " + ircmsg[ircmsg.find(":") + 1])
