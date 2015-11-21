fc86f9e658ec9613cba6a93cf99d9cd0afd00cc6
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-11-21 14:54:01 +0100

    Added strs to classes Group and Server

--- scripts/server.py
+++ scripts/server.py
@@ -1,9 +1,18 @@
 __author__ = 'Harald'
 
+
 class Server(object):
     def __init__(self, owner, hostname, prettyname, statusgroup, notifygroup):
         self.owner = owner
         self.hostname = hostname
         self.prettyname = prettyname
-        self.statusgroups = statusgroup
+        self.statusgroup = statusgroup
         self.notifygroup = notifygroup
+
+    def __str__(self):
+        string = "Server: " + "owner=" + self.owner + " " + self.hostname + " " + self.prettyname + " " + self.statusgroup + " " + self.notifygroup
+        return string
+
+
+    def __repr__(self):
+        return self.__str__()
