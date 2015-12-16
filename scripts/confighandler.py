aa8d7fbcffdcb706b7a44d9339ab88d6a365e5bd
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-12-16 08:37:25 +0100

    cfg file extension

--- scripts/confighandler.py
+++ scripts/confighandler.py
@@ -31,17 +31,17 @@ def readdata(filestring):
 
 def readgroups():
     groups = {}
-    readfile = readdata('config/groups')
+    readfile = readdata('config/groups.cfg')
     for dataobject in readfile:
         groups[dataobject.ident[0]] = Group(dataobject.ident[0], dataobject.ident[1],
                                             dataobject.data)
     return groups
 
 
-# read servers
+# read servers.cfg
 def readservers():
     servers = {}
-    readfile = readdata('config/servers')
+    readfile = readdata('config/servers.cfg')
     for dataobject in readfile:
         servers[dataobject.ident[0]] = Server(dataobject.ident[0], dataobject.ident[1],
                                               dataobject.data[0], dataobject.data[1],
@@ -62,10 +62,10 @@ def addusertogroup(user, groupname, recipient):
 
 
 def saveconfig():
-    configfile = open('config/servers', 'w')
+    configfile = open('config/servers.cfg', 'w')
     for host, serverobject in servers.items():
         configfile.write(serverobject.__str__() + '\n')
-    configfile = open('config/groups', 'w')
+    configfile = open('config/groups.cfg', 'w')
     for groupname, groupobject in groups.items():
         configfile.write(groupobject.__str__() + '\n')
 
